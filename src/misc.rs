65bac0818e0dc3baac47d35053d9983bda8659eb
Miner3DGaming <105589865+Miner3DGaming@users.noreply.github.com>
2025-03-22 18:07:00 +0100

    👍

--- src/misc.rs
+++ src/misc.rs
@@ -1,8 +1,8 @@
-
-
 pub fn concatenate<T: AsRef<str>>(input1: T, input2: T) -> String {
     let str1 = input1.as_ref();
     let str2 = input2.as_ref();
     format!("{}{}", str1, str2)
 }
-
+// pub fn concatenate2(input1: &str, input2: &str) -> String {
+//     format!("{}{}", input1, input2)
+// }
--- src/time.rs
+++ src/time.rs
@@ -1,13 +1,13 @@
-
 use std::time::{Duration, SystemTime};
-
-
 
 pub fn sleep_sec(ms: u64) {
     std::thread::sleep(Duration::from_secs(ms));
 }
 pub fn sleep_ms(ms: u64) {
     std::thread::sleep(Duration::from_millis(ms));
+}
+pub fn sleep_us(ms: u64) {
+    std::thread::sleep(Duration::from_micros(ms));
 }
 pub fn sleep_ns(ns: u64) {
     std::thread::sleep(Duration::from_nanos(ns))
@@ -29,11 +29,31 @@ pub fn get_elapsed_as_ns(time: SystemTime) -> u128 {
         }
     }
 }
+pub fn get_elapsed_as_us(time: SystemTime) -> u128 {
+    let elapsed = time.elapsed();
+
+    match elapsed {
+        Ok(duration) => {
+            return duration.as_micros();
+        }
+        Err(_) => {
+            print!("Error while getting time");
+            panic!();
+        }
+    }
+}
 
 pub fn wait_for_fps_ns(fps: u128, delta_time: u128) {
     let target_frame_time = 1_000_000_000 / fps; // Time for one frame at the target FPS
     if delta_time < target_frame_time {
         let sleep_time = target_frame_time - delta_time;
         sleep_ns(sleep_time as u64);
+    }
+}
+pub fn wait_for_fps_us(fps: u128, delta_time: u128) {
+    let target_frame_time = 1_000_000 / fps; // Time for one frame at the target FPS
+    if delta_time < target_frame_time {
+        let sleep_time = target_frame_time - delta_time;
+        sleep_us(sleep_time as u64);
     }
 }
