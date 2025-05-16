9e75fcba9cc9ac16f8da81489bd74ebe1a30fd2f
Miner3DGaming <105589865+Miner3DGaming@users.noreply.github.com>
2025-05-16 19:30:14 +0200

    Removed old struct

--- src/lists.rs
+++ src/lists.rs
@@ -1,30 +1,3 @@
-use std::collections::VecDeque;
-
-pub struct FixedSizeQueue<T> {
-    deque: VecDeque<T>,
-    max_size: usize,
-}
-
-impl<T> FixedSizeQueue<T> {
-    fn new(max_size: usize) -> Self {
-        Self {
-            deque: VecDeque::with_capacity(max_size),
-            max_size,
-        }
-    }
-
-    fn add(&mut self, item: T) {
-        if self.deque.len() == self.max_size {
-            self.deque.pop_front(); // Remove the oldest item
-        }
-        self.deque.push_back(item); // Add the new item
-    }
-
-    fn as_vec(&self) -> Vec<&T> {
-        self.deque.iter().collect()
-    }
-}
-
 pub fn add_item_to_max_sized_list<T>(
     list: &mut Vec<T>,
     max_size: usize,
