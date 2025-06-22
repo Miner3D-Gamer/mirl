4b6b7c923290fbe92ac3e307297f637992e61a32
Miner3DGaming <105589865+Miner3DGaming@users.noreply.github.com>
2025-06-22 21:51:12 +0200

    Added own number system bc num-traits was insufficient for my needs

--- src/math.rs
+++ src/math.rs
@@ -19,3 +19,63 @@ pub fn normalize_vector(x: f32, y: f32, z: f32) -> (f32, f32, f32) {
     let v = (x * x + y * y + z * z).abs().sqrt();
     return (x / v, y / v, z / v);
 }
+
+use num_traits::NumCast;
+
+use crate::render::{U1, U2, U4};
+
+pub trait WholeNumber: std::cmp::PartialOrd + NumCast {}
+impl WholeNumber for U1 {}
+impl WholeNumber for U2 {}
+impl WholeNumber for U4 {}
+
+impl WholeNumber for u8 {}
+impl WholeNumber for u16 {}
+impl WholeNumber for u32 {}
+impl WholeNumber for u64 {}
+impl WholeNumber for u128 {}
+impl WholeNumber for usize {}
+
+impl WholeNumber for i8 {}
+impl WholeNumber for i16 {}
+impl WholeNumber for i32 {}
+impl WholeNumber for i64 {}
+impl WholeNumber for i128 {}
+impl WholeNumber for isize {}
+
+pub trait FloatNumber: std::cmp::PartialOrd + NumCast {}
+
+impl FloatNumber for f32 {}
+impl FloatNumber for f64 {}
+
+pub trait Number:
+    std::cmp::PartialOrd
+    + NumCast
+    + std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Mul<Output = Self>
+    + std::ops::Div<Output = Self>
+{
+}
+
+
+impl Number for U1 {}
+impl Number for U2 {}
+impl Number for U4 {}
+
+impl Number for u8 {}
+impl Number for u16 {}
+impl Number for u32 {}
+impl Number for u64 {}
+impl Number for u128 {}
+impl Number for usize {}
+
+impl Number for i8 {}
+impl Number for i16 {}
+impl Number for i32 {}
+impl Number for i64 {}
+impl Number for i128 {}
+impl Number for isize {}
+
+impl Number for f32 {}
+impl Number for f64 {}
